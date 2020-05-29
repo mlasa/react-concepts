35e76edfe11fe5e7e59e85922704b93af8aa3d72
Marcella LASA <marcella.amorimsa@gmail.com>
2020-05-28 21:56:24 -0300

    state and immutability

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,21 @@ import React from "react";
 import Header from "./Header";
 
 export default function App() {
+  const title = "Marcella";
   return (
     <>
-      <Header title="Homepage" />
-      <Header title="Projects ReactJS" />
+			<Header title={title}>
+				<ul>
+					<li>Idade:22</li>
+					<li>Campinas-SP</li>
+				</ul>
+			</Header>
+			<Header title="Projects">
+				<ul>
+					<li>BB8</li>
+					<li>Jeany</li>
+				</ul>	
+			</Header>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header(props) {
   return (
     <header>
       <h1>{props.title}</h1>
+      {props.children}
     </header>
   );
 }
